a97835687e97fdb44aa142c18c421a1c1a0853ca
ruioliveira02 <rpoliveira02@gmail.com>
2022-02-10 19:37:50 +0000

    Signup state

--- pages/signup.js
+++ pages/signup.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import { withoutAuth } from "/components/Auth";
 import Fade from "react-reveal/Fade";
 
@@ -12,19 +14,31 @@ import Title from "/components/moonstone/authentication/Title";
 import Text from "/components/moonstone/authentication/Text";
 
 function Signup() {
+
+  const [name, updateName] = useState("");
+  const [email, updateEmail] = useState("");
+  const [username, updateUsername] = useState("");
+  const [password, updatePassword] = useState("");
+  const [passwordConf, updatePasswordConf] = useState("");
+
+  const onFinish = e => {
+    e.preventDefault();
+  };
+
   return (
     <div className="min-h-screen overflow-hidden bg-secondary">
       <Return componentStyle="sm:ml-14 mt-10 sm:mt-20 mb-6" />
       <div className="flex flex-col items-center justify-center sm:mt-16">
         <Title text="Sign up" />
 
-        <Form>
+        <Form onSubmit={onFinish}>
           <Input
             text="NAME"
             id="name"
             name="name"
             fgColor="white"
             bgColor="primary"
+            onChange={e => updateName(e.currentTarget.value)}
           />
           <Input
             text="EMAIL"
@@ -34,6 +48,7 @@ function Signup() {
             autoComplete="email"
             fgColor="white"
             bgColor="primary"
+            onChange={e => updateEmail(e.currentTarget.value)}
           />
           <Input
             text="USERNAME"
@@ -41,6 +56,7 @@ function Signup() {
             name="username"
             fgColor="white"
             bgColor="primary"
+            onChange={e => updateUsername(e.currentTarget.value)}
           />
           <Input
             text="PASSWORD"
@@ -50,6 +66,7 @@ function Signup() {
             autoComplete="current-password"
             fgColor="white"
             bgColor="primary"
+            onChange={e => updatePassword(e.currentTarget.value)}
           />
           <Input
             text="CONFIRM PASSWORD"
@@ -59,6 +76,7 @@ function Signup() {
             autoComplete="current-password"
             fgColor="white"
             bgColor="primary"
+            onChange={e => updatePasswordConf(e.currentTarget.value)}
           />
           <Button
             type="submit"
